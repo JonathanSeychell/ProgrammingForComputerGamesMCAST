3440b9688e589aed4ba16b8baa65d93059a6565f
JonathanSeychell <56071281+JonathanSeychell@users.noreply.github.com>
2019-10-17 13:13:28 +0200

    17/10/19

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    Vector2 paddlePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       paddlePosition = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        //Limit the mouse position between 1 and 15 units
+        float limitMousePos = Mathf.Clamp(mousePos, 1f, 15f);
+
+        //set the Paddle x-position to the x-mousePosition
+        paddlePosition.x = limitMousePos;
+        this.transform.position = paddlePosition;
+    }
+}
