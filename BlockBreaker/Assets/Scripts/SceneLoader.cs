7678c4ffdfee81b654cf540cec53c58b8e84259d
JonathanSeychell <56071281+JonathanSeychell@users.noreply.github.com>
2019-10-24 13:11:06 +0200

    24-10-19
    buttons made

---
+++ BlockBreaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // a method which loads the Scene: SceneName
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Quit(string msg)
+    {
+        if (msg == "quit")
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
