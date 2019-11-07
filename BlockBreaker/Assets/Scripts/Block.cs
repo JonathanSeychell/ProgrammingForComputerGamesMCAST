6815bd06adb54b0ee306c453843f5a09d26ef93f
JonathanSeychell <56071281+JonathanSeychell@users.noreply.github.com>
2019-11-07 13:18:40 +0100

    07/11/19

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -7,34 +7,65 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
 
+    [SerializeField] int maxHits;
+    [SerializeField] int currentHits = 0;
+
+    [SerializeField] Sprite[] hitSprites;
+
     Level level;
 
     private void Start()
     {
         level = FindObjectOfType<Level>();
+        if (gameObject.tag == "Breakable")
+        {
+            level.CountBreakableBlocks();
 
-        level.CountBreakableBlocks();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        FindObjectOfType<GameStatus>().AddToScore();
 
         //play audioclip at camera's position
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
 
+        if (gameObject.tag == "Breakable")
+        {
+
+            currentHits++;
+            if (currentHits >= maxHits)
+            {
+
+                FindObjectOfType<GameStatus>().AddToScore();
+                DestroyBlock();
+            }
+            else
+            {
+                showNextHitSprite();
+
+            }
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
         level.BlockDestroyed();
-
-
         Destroy(gameObject);
         TriggerSparklesVFX();
     }
-
 
     private void TriggerSparklesVFX()
     {
         GameObject sparkles = Instantiate(blockSparklesVFX, this.transform.position, this.transform.rotation);
         Destroy(sparkles, 1f);
+    }
+
+    private void showNextHitSprite()
+    {
+        int spriteIndex = currentHits - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 }
