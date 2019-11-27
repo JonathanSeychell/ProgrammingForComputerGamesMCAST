a52c887f00eaece1f0f02447aa59ef61e83b006f
JonathanSeychell <56071281+JonathanSeychell@users.noreply.github.com>
2019-11-27 12:49:58 +0100

    27/11/19

---
+++ LaserDefenderSWD42B/Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    
+    int waypointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();   
+    }
+    private void EnemyMovement()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].transform.position;
+            targetPosition.z = 0f;
+
+            float movementPerFrame = moveSpeed *  Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position,targetPosition, movementPerFrame);
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ LaserDefenderSWD42B/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] int maximumEnemies = 10;
+    [SerializeField] float spawnTime = 1.5f;
+    int currentEnemies = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnEnemies()
+    {
+        while (currentEnemies <= maximumEnemies)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, spawnPoint.transform.position, transform.rotation);
+            currentEnemies++;
+
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
